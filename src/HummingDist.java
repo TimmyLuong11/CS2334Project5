b94ebb526901b19e385d6f513308da8c7b6799a1
Timmy <timothy.luong@ou.edu>
2019-04-30 15:35:48 -0500

    Code

--- src/HummingDist.java
+++ src/HummingDist.java
@@ -2,13 +2,18 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -28,9 +33,9 @@ public class HummingDist extends JFrame
 	private JSlider slider;	
 	private JTextField inputTextField;
 	private JPanel panel0;
-	private JPanel panel1;
-	private JPanel panel2;
-	private JPanel panel3;
+	//private JPanel panel1;
+	//private JPanel panel2;
+	//private JPanel panel3;
 	private JComboBox<String> list;
 	private JLabel label1;
 	private JLabel label2;
@@ -46,16 +51,20 @@ public class HummingDist extends JFrame
 	private JLabel dist3;
 	private JLabel dist4;
 	private JLabel showStationBox;
+	private JLabel yes;
 	private Color lb = new Color(51,153,225);
+	private BufferedImage img;
+	private JLabel pic;
+	private JLabel pic1;
 	
 	public HummingDist() throws IOException
 	{
 		initFrame();
 		setButton();
 		setSlider();
-		setTextField();
 		setComboBox();
 		setText();
+		setImage();
 		panel0.add(b1);
 		panel0.add(b2);
 		panel0.add(b3);
@@ -76,22 +85,39 @@ public class HummingDist extends JFrame
 		panel0.add(dist4);
 		panel0.add(valueTextField);
 		panel0.add(showStationBox);
-		//panel0.add(panel1);
-		//panel0.add(panel2);
+		panel0.add(pic);
+		panel0.add(pic1);
+		panel0.add(yes);
 		this.add(panel0);
 	}
 
-	 
+	private void setImage() throws IOException 
+	{
+		img = ImageIO.read(new File("pro.jpg"));
+		pic = new JLabel(new ImageIcon(img));
+		pic.setBounds(225, 15, 200, 200);
+		img = ImageIO.read(new File("pic1.jpg"));
+		pic1 = new JLabel(new ImageIcon(img));
+		pic1.setBounds(300, 250, 200, 200);
+	}
 
 	private void setText() 
 	{
+		yes = new JLabel();
+		yes.setText("Prof: Say Yes  Me: Yes");
+		yes.setBounds(400, 600, 130, 100);
+		
+		inputTextField = new JTextField(10);
+		inputTextField.setText("ZERO");
+		inputTextField.setBounds(150, 700, 100, 25);
+
 		label1 = new JLabel();
 		label1.setText("Enter Hamming Dist:");
-		label1.setBounds(5, 8, 120, 10);
+		label1.setBounds(10, 8, 120, 10);
 		
 		label2 = new JLabel();
 		label2.setText("Compare with:");
-		label2.setBounds(5, 415, 120, 10);
+		label2.setBounds(10, 415, 120, 10);
 
 		label3 = new JLabel();
 		label3.setText("Distance 0");
@@ -135,9 +161,6 @@ public class HummingDist extends JFrame
 		showStationBox.setBounds(25, 150, 200, 250);
 	}
 	
-
-
-
 	private void setComboBox() throws IOException
 	{
 		BufferedReader input = new BufferedReader(new FileReader("Mesonet.txt"));
@@ -170,24 +193,28 @@ public class HummingDist extends JFrame
 		
 	}
 
-	private void setTextField() 
-	{
-		inputTextField = new JTextField(10);
-		inputTextField.setBounds(150, 700, 115, 25);
-	}
-
 	private void setButton() 
 	{
 		b1 = new JButton("b1");
 		b1.setText("Show Station");
-		b1.setBounds(15, 100, 115, 25);
+		b1.setBounds(17, 100, 115, 25);
 		b2 = new JButton("b2");
-		b2.setBounds(15, 450, 115, 25);
+		b2.setBounds(17, 450, 115, 25);
 		b2.setText("Calculate HD");
 		b3 = new JButton("b3");
-		b3.setBounds(15, 700, 115, 25);
+		b3.setBounds(17, 700, 115, 25);
 		b3.setText("Add Station");
 		setVisible(true);
+		
+		b1.addActionListener(new ActionListener()
+		{	
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
 		b2.addActionListener(new ActionListener()
 		{	
@@ -218,7 +245,8 @@ public class HummingDist extends JFrame
 		slider.setPaintLabels(true);
 		valueTextField = new JLabel();
 		valueTextField.setBorder(BorderFactory.createLineBorder(lb, 1));
-		valueTextField.setBounds(125, 5, 100, 20);
+		valueTextField.setBounds(130, 5, 100, 20);
+		valueTextField.setText(" " + slider.getValue());
 		slider.addChangeListener(new ChangeListener()
 		{
 			@Override
@@ -227,7 +255,6 @@ public class HummingDist extends JFrame
 				valueTextField.setText(" " + slider.getValue());
 			}
 		});
-		
 	}
 
 	private void initFrame()
